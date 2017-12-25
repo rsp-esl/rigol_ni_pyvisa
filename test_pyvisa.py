ebc24ef5dcce0c8f914e995ff72a47ca8f05b45c
rsp <siripokarpirom@gmail.com>
2017-12-25 18:35:05 +0700

    Add files via upload

--- test_pyvisa.py
+++ test_pyvisa.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
 ############################################################################
-# Date: 2017-11-14
+# Date: 2017-12-25
 ############################################################################
-# Author: Rawat S. 
+# Author: Rawat S.
 #   (Dept. of Electrical & Computer Engineering, KMUTNB, Bangkok/Thailand)
 #
 ############################################################################
 # Short Description:
-#   This Python script is used to test the USB connection to 
-#   Rigol digital oscilloscopes (such as DS2072A) 
+#   This Python script is used to test the USB connection to
+#   Rigol digital oscilloscopes (such as DS2072A)
 #   or function generators (DG1022).
-#   It relies on the NI VISA interface / driver. 
+#   It relies on the NI VISA interface / driver.
 #   (Make sure that the proper NI VISA driver has been installed correctly.)
 #
 ############################################################################
 # Usage:
 #   Don't forget to install pyusb and pyvisa packages:
 #   (When using Linux/Ubuntu, don't forget to use the sudo command).
-# 
+#
 #   $ pip install pyusb -U
 #   $ pip install pyvisa -U
 #   $ pip install pyvisa-py -U
@@ -26,27 +26,31 @@
 #   To run the script under Linux (e.g. Ubuntu)
 #   (When using Linux/Ubuntu, don't forget to use the sudo command).
 #
-#   $ python2  ./test_pyvisa.py
-#   $ python3  ./test_pyvisa.py
+#   $ sudo python2  ./test_pyvisa.py
+#   $ sudo python3  ./test_pyvisa.py
 #
 ############################################################################
 
 import visa
 import time, sys
 
-visa_driver = ''  #  use either 'visa64' or 'visa32' or '@py' or left empty. 
-resources = visa.ResourceManager( visa_driver ) 
+visa_driver = '@py'  #  use either 'visa64' or 'visa32' or '@py' or left empty.
+resources = visa.ResourceManager( visa_driver )
 devices = resources.list_resources()
+selected_device = None
 
 if len(devices) > 0:
     print ('Found #devices: %d' % len(devices) )
     for device in devices:
         print ('>>', device)
-        
-print (30*'-')
+        if selected_device == None and str(device).startswith('USB'):
+            selected_device = device
+            print ('selected:', selected_device)
 
-if len(devices) > 0:
-    instr = resources.open_resource( devices[0] ) # select the first device found
+print (50*'-')
+
+if selected_device != None:
+    instr = resources.open_resource( selected_device ) # select the first device found
     instr.write( '*IDN?' )
     ret_str = instr.read()
     fields = ret_str.split(',')
